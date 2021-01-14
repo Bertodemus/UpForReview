8655653b89d62a159d059b1b4ceaebde0b0767bf
Bertodemus <roberto@robertorubet.com>
2021-01-13 22:15:22 -0500

    cleaned up code

--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-// import Card from "../components/Card";
-// import FriendCard from "../components/FriendCard";
 import Hero from "../components/Hero";
 import Container from "../components/Container";
 import Row from "../components/Row";
@@ -10,16 +8,16 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
 
 function cellImage(cell, row) {
-    return (
-      <div>
-        <img src={cell}></img>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <img src={cell}></img>
+    </div>
+  );
+}
 
 const columns = [{
   dataField: 'picture.thumbnail',
-  text: 'Photo',
+  text: '',
   align: 'center',
   formatter: cellImage,
   headerStyle: (colum, colIndex) => {
@@ -65,8 +63,6 @@ class Employees extends Component {
   state = {
     employees: []
   };
-
-  // // When the component mounts, load the employees to be displayed
   componentDidMount() {
     this.loadAllEmployees();
   }
@@ -82,33 +78,28 @@ class Employees extends Component {
   };
 
   render() {
-    console.log(this.state.employees);
-      return (
-        <div>
-          <Hero backgroundImage="/assets/images/ufr.png">
-            {/* <h1>Up For Review</h1>
-            <h2>All your employees in a single view!</h2> */}
-          </Hero>
-          <Container style={{ marginTop: 30 }}>
-            <Row>
-              <Col size="md-12">
-                <h2>Welcome To UpForReview!</h2>
-              </Col>
-            </Row>
-            <Row>
-              <Col size="md-12">
-
-                <p>
-                  UFR is a small table with a dash of functionality that allows you to view non-personal data of your current employees. The table columns can be sorted by ascending or descending order. In order to filter, simply type into the desired column you wish to filter. It will instantly update with the results.
-                </p>
-
-              <BootstrapTable keyField='id' data={ this.state.employees } columns={ columns } filter={ filterFactory() } striped hover condensed/>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      );
-    };
+    return (
+      <div>
+        <Hero backgroundImage="/assets/images/ufr.png">
+        </Hero>
+        <Container style={{ marginTop: 30 }}>
+          <Row>
+            <Col size="md-12">
+              <h2>Welcome To UpForReview!</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-12">
+              <p>
+                UFR is a small table with a dash of functionality that allows you to view non-personal data of your current employees. The table columns can be sorted by ascending or descending order. To filter, simply type into the desired column you wish to filter. It will instantly update with the results.
+              </p>
+            <BootstrapTable keyField='id' data={ this.state.employees } columns={ columns } filter={ filterFactory() } striped hover condensed/>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  };
 }
 
 export default Employees;
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
-
 export default {
   getAllEmployees: function() {
     return axios.get("https://randomuser.me/api/?results=10");
